c88a19f0485890c79b90929095bbd87d89614d2a
ravindu <ravindulakshitha250@gmail.com>
2024-05-15 19:21:35 +0530

    change button name

---
+++ skillsync/src/components/submitbutton/submitbutton.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './button.css';
+
+export default function Button() {
+  return (
+    <>
+    <div className='heroLoginbtn'>
+                <button type="button">Submit</button>
+            </div>
+    </>
+  );
+};
