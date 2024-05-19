8741ead1c2c3af4507388072117e7a9d8023e38b
ravindu <ravindulakshitha250@gmail.com>
2024-05-19 09:50:21 +0530

    solved complit Hero page

--- skillsync/src/components/Hero/Hero.jsx
+++ skillsync/src/components/Hero/Hero.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 import "./Hero.css";
 import NavBar from "../NavBar/Lognavbar";
@@ -19,27 +18,6 @@ export const Hero = () => {
       <div className="heroLoginbtn">
         <button type="button">Sign in</button>
       </div>
-=======
-import React from 'react'
-import './Hero.css'
-import NavBar from '../NavBar/Lognavbar'
-import { Link } from 'react-router-dom'
-
-
-export const Hero = () => {
-    return (
-          <div className="hero-left">
-            <div className='lnavbar-container'>
-            <NavBar/>
-            </div>
-            <h1>Simplify Your Internship Search with InternX.</h1>
-            <p>SkillSync revolutionizes the internship search process in the dynamic IT industry.
-                Discover your perfect internship effortlessly, while IT companies find top talent seamlessly.
-                Join InternX today and unlock boundless opportunities.</p>
-            <div className='heroLoginbtn'>
-              <Link to="/signin"><button type="button">Sign in</button></Link>
-            </div>
->>>>>>> main
     </div>
   );
 };
