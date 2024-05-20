a16965da478e0fb72746e6c2968e9792c25cebd6
Lakshika Bnadara <lakshikabandara601@gmail.com>
2024-05-20 17:37:17 +0530

    Craet create acc page

--- skillsync/src/Pages/CreateAcc/CreateAcc.jsx
+++ skillsync/src/Pages/CreateAcc/CreateAcc.jsx
@@ -4,48 +4,51 @@ import "./CreateAcc.css";
 export const CreateAcc = () => {
   return (
     <>
-    <div className="flex-container">
-      <div className="flex-box1">
-        <div className="logo"></div>
-        <div className="welcome">Welcome to</div>
-        <div className="internx">InternX Community</div>
-      </div>
-      <div className="flex-box2">
-        <div className="createAcc">
-          <h5>Create a InternX acount</h5>
-          <div className="input-box">
-            <form action="past">
-              <input type="text" placeholder="Full Name" required />
-            </form>
+      <div className="flex-Container">
+        <div className="flex-box01">
+          <div className="logo">logo</div>
+          <div className="welcome">Welcome to</div>
+          <div className="internx">InternX Community</div>
+        </div>
+        <div className="flex-box02">
+          <div className="createAcc">
+            <h5>Create a InternX account</h5>
+            <div className="input-box1">
+              <form action="past">
+                <input type="text" placeholder="Full Name" required />
+              </form>
+            </div>
+            <div className="input-box1">
+              <form action="past">
+                <input type="text" placeholder="Email" required />
+              </form>
+            </div>
+            <div className="input-box1">
+              <form action="past">
+                <input type="password" placeholder="Your Password" required />
+              </form>
+            </div>
+            <div className="terms">
+              <label>
+                <input type="checkbox" />I agree with InternX Terms of Services
+                and Privacy Policy.
+              </label>
+            </div>
+            <button type="submit" id="signupButton">
+              Sign up
+            </button>
+            <div class="divider-container">
+              <div class="divider"></div>
+              <div class="divider-text">or</div>
+              <div class="divider"></div>
+            </div>
+            <button className="google-button" type="submit" id="googleButton">
+             <img src="https://logos-world.net/wp-content/uploads/2020/09/Google-Symbol.png" alt="Google logo"></img>
+              Continue with google
+            </button>
           </div>
-          <div className="input-box">
-            <form action="past">
-              <input type="text" placeholder="Email" required />
-            </form>
-          </div>
-          <div className="input-box">
-            <form action="past">
-              <input type="password" placeholder="Your Password" required />
-            </form>
-          </div>
-          <div className="terms">
-            <label>
-              <input type="checkbox" />I agree to InternX Terms of Services and
-              Privacy Policy.
-            </label>
-          </div>
-          <button type="submit" id="signupButton">
-            Sign up
-          </button>
-          <div className="or">
-            <p>---------------------------- OR ---------------------------</p>
-          </div>
-          <button className="google-button" type="submit" id="googleButton">
-            Continue with google
-          </button>
         </div>
       </div>
-    </div>
     </>
   );
 };
