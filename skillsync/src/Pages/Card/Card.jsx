be16500b8440500e4d5c928993b9b14ae5e7d2ca
Lakshika Bnadara <lakshikabandara601@gmail.com>
2024-06-08 12:50:12 +0530

    Create Card file

---
+++ skillsync/src/Pages/Card/Card.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Card.css";
+import Lnavbar from "../../components/NavBar/Lognavbar";
+
+export const Card = () => {
+    return (
+        <>
+        
+        </>
+    )
+}
+export default Card;
--- skillsync/src/components/Footer/footer.jsx
+++ skillsync/src/components/Footer/footer.jsx
@@ -3,18 +3,18 @@ import "./footer.css";
 function Footer() {
     return (<footer>
         <div class="row">
-            <div class="col">
+            <div class="Fcol">
             <img src="logo.jpg" class="logo"alt="" />
             <p>InternX revolutionizes the internship search process in the dynamic IT industry. Discover your perfect internship effortlessly, while IT companies find top talent seamlessly. Join InternX today and unlock boundless opportunities.</p>
         </div>
-        <div class="col">
+        <div class="Fcol">
             <h3>Office<div class="underline"><span></span></div></h3>
             <p>Dharmapala Mawatha</p>
             <p>Colombo 10, Sri Lanka</p>
             <p class="email-id">internx@gmail.com</p>
             <h4>011-3476910</h4>
         </div>
-            <div class="col">
+            <div class="Fcol">
                 <h3>Links<div class="underline"><span></span></div></h3>
                 <ul>
                     <li><a href= "">Home</a></li>
@@ -24,7 +24,7 @@ function Footer() {
                      <li><a href= "">Contacts</a></li>    
                 </ul>
             </div>
-            <div class="col">
+            <div class="Fcol">
                 <h3>Newsletter<div class="underline"><span></span></div></h3>
                 <form>
                     <i class="fa-regular fa-envelope"></i>
