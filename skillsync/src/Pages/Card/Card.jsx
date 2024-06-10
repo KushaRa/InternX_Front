6135eb91d5c8be0fb302019398b2ae7c5935dbfc
Lakshika Bnadara <lakshikabandara601@gmail.com>
2024-06-11 00:40:03 +0530

    created the search button

--- skillsync/src/Pages/Card/Card.jsx
+++ skillsync/src/Pages/Card/Card.jsx
@@ -1,12 +1,22 @@
 import React from "react";
 import "./Card.css";
 import Lnavbar from "../../components/NavBar/Lognavbar";
+import { Link } from 'react-router-dom';
 
 export const Card = () => {
     return (
-        <>
+        <div className="cardPage">
+            <header classname="cardHeader">
+            <Lnavbar/>
+            </header>
+            <h1 className="cardHeader">Unlock endless<br/ > possibilities for your<br/ > future career</h1>
+            <div className="cardSearchBar">
+                <input type= "text" placeholder="Type Here"/>
+                <button className="cardButton">Search</button> 
+            </div>
+        </div>
         
-        </>
+        
     )
 }
 export default Card;
