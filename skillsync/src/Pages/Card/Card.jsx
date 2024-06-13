739bc7b8a329df0cb3e93f73b745992aa6f3c2a3
kushana <rashmisenadheera98@gmail.com>
2024-06-13 16:55:33 +0530

    change some styles in card page

--- skillsync/src/Pages/Card/Card.jsx
+++ skillsync/src/Pages/Card/Card.jsx
@@ -4,40 +4,38 @@ import MainNav from '../../components/MainNav/MainNav';
 
 export const Card = () => {
     return (
-        <div className="cardPage">
-            <header className="cardHeader">
+        <>
+    <div className="cardPage"> 
+        <div className='Mnavbar-container'>
             <MainNav/>
-            </header>
-            <section className="card-section">
-            <h1 className="cardHeader">Unlock endless<br/ > possibilities for your<br/ > future career</h1>
-            <div className="cardSearchBar">
-                <input type="text" placeholder="Type Here"/>
-                <button className="cardButton">Search</button> 
             </div>
-            </section>
-            <section className="card-featured-internships">
-                <h2>Featured Internship Offers</h2>
-                <div className="internship-cards">
-                    {Array(6).fill().map((_, index) => (
-                        <div className="internship-card" key={index}>
-                        <h3>Delivergate (Private) Limited</h3>
-                        <p>Front-End Developer Intern</p>
-                        <div className="card-details">
-                            <span>Colombo</span>
-                            <span>Remote</span>
-                            <span>6 Months</span>
-              </div>
+                 <section className="card-section">
+                    <h1 className="cardHeader">Unlock Endless<br/> Possibilities For Your<br/> Future Career</h1>
+                    <div className="cardSearchBar">
+                        <input type="text" placeholder="Type Here"/>
+                        <button className="cardButton">Search</button> 
+                    </div>
+                </section>
+                <section className="card-featured-internships">
+                    <h2>Featured Internship Offers</h2>
+                    <div className="internship-cards">
+                        {Array(6).fill().map((_, index) => (
+                            <div className="internship-card" key={index}>
+                                <h3>Delivergate (Private) Limited</h3>
+                                <p>Front-End Developer Intern</p>
+                                <div className="card-details">
+                                    <span>Colombo</span>
+                                    <span>Remote</span>
+                                    <span>6 Months</span>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                    <a href="#" className="all-job-offers">All Job Offers</a>
+                </section>
             </div>
-          ))}
-        </div>
-        <a href="#" className="all-job-offers">All Job Offers</a>
-                
-
-
-            </section>
-        </div>
-        
-        
-    )
+        </>
+    );
 }
+
 export default Card;
