50e001d6445b45b82bebb256824693ff153b4aea
ravindu <ravindulakshitha250@gmail.com>
2024-06-14 22:41:57 +0530

    remove unwanted files.

--- skillsync/src/App.js
+++ skillsync/src/App.js
@@ -1,6 +1,7 @@
 // import { CreateAcc } from './Pages/CreateAcc/CreateAcc';
 import React from "react";
 
+
 import { Home } from "./Pages/Home/Home";
 import { AboutUs } from "./Pages/AboutUs/AboutUs";
 import { Contact } from "./Pages/Contact/Contact";
