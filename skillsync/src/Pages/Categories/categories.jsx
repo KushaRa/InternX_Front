6211b309226e9401f560e6f95aee0168b23415ae
ravindu <ravindulakshitha250@gmail.com>
2024-06-17 00:19:53 +0530

    update structure

--- skillsync/src/Pages/Categories/categories.jsx
+++ skillsync/src/Pages/Categories/categories.jsx
@@ -1,8 +1,14 @@
-import React from 'react';
-import './categories.css';
+import React from "react";
+import "./categories.css";
+import MainNav from "../../components/MainNav/MainNav";
+import "../../components/MainNav/MainNav.css";
 
 export const Categories = () => {
   return (
-    <div></div>
-  )
-}
+    <div>
+      <div className="category_page">
+        <MainNav />
+      </div>
+    </div>
+  );
+};
