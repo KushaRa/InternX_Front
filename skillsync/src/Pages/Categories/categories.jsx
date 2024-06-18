d2a117287951409424c3da4fc7fd61c520f43d51
ravindu <ravindulakshitha250@gmail.com>
2024-06-18 18:27:14 +0530

    Add flexbox to page

--- skillsync/src/Pages/Categories/categories.jsx
+++ skillsync/src/Pages/Categories/categories.jsx
@@ -7,6 +7,11 @@ export const Categories = () => {
   return (
     <div className="category_page">
       <MainNav />
+      <div className="flex-container">
+        <div className="flex-box3">
+        </div>
+        <div className="flex-box4"></div>
+      </div>
     </div>
   );
 };
