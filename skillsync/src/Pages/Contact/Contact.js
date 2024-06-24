c6bab099a4b664d9290471c59f82f46b893b9ab9
Lakshika Bnadara <lakshikabandara601@gmail.com>
2024-06-24 22:27:04 +0530

    changed the format and css of the contact page

--- skillsync/src/Pages/Contact/Contact.js
+++ skillsync/src/Pages/Contact/Contact.js
@@ -2,71 +2,42 @@ import React from 'react';
 import "./Contact.css";
 import MainNav from "../../components/MainNav/MainNav";
 // import Footer from '../../components/Footer/footer';
-import address from "../../assets/address.png";
-import email from "../../assets/email.png";
-import phone from "../../assets/phone.png";
+// import address from "../../assets/address.png";
+// import email from "../../assets/email.png";
+// import phone from "../../assets/phone.png";
 
 
 export const Contact = () => {
     return (
-        <div className='contact-page'>
-            <MainNav />
-            <section className='contact-section'>
-                <div className='contact-content'>
-                    <h2>Contact Us</h2>
-                    <p>Get in Touch</p>
-                </div>
-                <div className='contact-container'>
-                    <div className='contact-info'>
-                        <div className='contact-box'>
-                            <div className='contact-icon'><img className="address-logo " src={address} alt="Logo" /> </div>
-                            <div className='contact-text'></div>
-                            <h3>Address</h3>
-                            <p>Dharmapala Mawatha,</p>
-                            <p>Colombo 10,</p>
-                            <p>Sri Lanka</p>
-                        </div>
-                        <div className='contact-box'>
-                            <div className='contact-icon'><img className="email-logo " src={email} alt="Logo" /> </div>
-                            <div className='contact-text'></div>
-                            <h3>Email</h3>
-                            <p>internx@gmail.com</p>
-                        </div>    
-                        <div className='contact-box'>
-                            <div className='contact-icon'><img className="phone-logo " src={phone} alt="Logo" /> </div>
-                            <div className='contact-text'></div>
-                            <h3>Phone</h3>
-                            <p>011-3476910</p>
-                        </div>   
+        // <div className='contact-page'>
+            
+            <div className='contact-container'>
+                <MainNav />
+                <h1>Contact Us</h1>
+                <div className='contat-content'>
+                    <div className='contacct-info'>
+                        <h2>Get in Touch</h2>
+                        <p>Address:Dharmapala Mawatha</p>
+                        <p>Colombo 10, Sri Lanka</p>
+                        <p>Email: internx@gmail.com</p>
+                        <p>Phone: 011-3476910</p>
                     </div>
                     <div className='contact-form'>
-                        <form>
-                            <h2>Send Message</h2>
-                            <div className='cntInput-box'>
-                                <input type='text' name='' required="required"></input>
-                                <span>Full Name</span>
-                            </div>
-                            <div className='cntInput-box'>
-                                <input type='text' name='' required="required"></input>
-                                <span>Email</span>
-                            </div>
-                            <div className='cntInput-box'>
-                                <textarea required="required"></textarea>
-                                <span>Type Your Message</span>
-                            </div>
-                            <div className='cntInput-box'>
-                                <input type='submit' name='' value="send"></input>
-                            </div>
-                        </form>
+                    <input type="text" placeholder="Name" />
+                    <input type="email" placeholder="Email" />
+                    <textarea placeholder="Message"></textarea>
+                    <button type="submit">Send</button>   
                     </div>
                 </div>
-            </section>
-            
+            </div>
+
+                
             
 
-            {/* <Footer /> */}
-        </div>
+            // {/* <Footer /> */}
+        // </div>
         
     )
     
 }
+export default Contact;
