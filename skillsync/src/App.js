1dafd88a84795bef06bc7a39ffc49ada3dd02f78
NethminiSandunika <nethminid92@gmail.com>
2024-06-26 19:06:11 +0530

    modify the company dashbord to display the company profile (name and the company information).

--- skillsync/src/App.js
+++ skillsync/src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { Home } from "./Pages/Home/Home";
-import { AboutUs } from "./Pages/AboutUs/AboutUs";
-import { Contact } from "./Pages/Contact/Contact";
+//import { AboutUs } from "./Pages/AboutUs/AboutUs";
+//import { Contact } from "./Pages/Contact/Contact";
 import { Company } from "./Pages/CompanyDashboard/Company";
-import  Login  from "./Pages/logIn/LogIn";
-import SelectUser from "./Pages/selectUser/SelectUser";
-import StudentProfile from "./Pages/studentProfile/StudentProfile";
-import Signup   from "./Pages/CreateAcc/CreateAcc";
+//import  Login  from "./Pages/logIn/LogIn";
+//import SelectUser from "./Pages/selectUser/SelectUser";
+//import StudentProfile from "./Pages/studentProfile/StudentProfile";
+//import Signup   from "./Pages/CreateAcc/CreateAcc";
 import { Route, Routes } from "react-router-dom";
 
 function App() {
@@ -15,13 +15,9 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />}></Route>
         {/*<Route path="/internships" element={<Internships/>}></Route>*/}
-        <Route path="/contact" element={<Contact />}></Route>
-        <Route path="/about" element={<AboutUs />}></Route>
-        <Route path="/login" element={<Login />}></Route>
+        
         <Route path="/internships" element={<Company />}></Route>
-        <Route path="/selectuser" element={<SelectUser />}></Route>
-        <Route path="/signup" element={<Signup />}></Route>
-        <Route path="/profile" element={<StudentProfile />} />
+     
       </Routes>
     </div>
 );
--- skillsync/src/Pages/CompanyDashboard/Company.jsx
+++ skillsync/src/Pages/CompanyDashboard/Company.jsx
@@ -1,20 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Company.css";
 import NavBar from "../../components/NavBar/Lognavbar";
 import { Popular } from "../../components/Popular/popular";
 
 export const Company = () => {
+  const [profile, setProfile] = useState({
+    name: "",
+    companyInformation: "",
+    profilePicture: null,
+  });
+
+  const [profilePicturePreview, setProfilePicturePreview] = useState(null);
+
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+    setProfile({ ...profile, profilePicture: file });
+
+    const reader = new FileReader();
+    reader.onloadend = () => {
+      setProfilePicturePreview(reader.result);
+    };
+    reader.readAsDataURL(file);
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setProfile({ ...profile, [name]: value });
+  };
+
   return (
     <div className="Content">
       <NavBar />
-
-      {/*<div className="CDetails"></div>*/}
-      <div className="InternCards">
-        <Popular />
-      </div>
-
-      <div className="NewIntern">
-            <button className="NewAdd">ADD NEW</button>
+      <div className="container">
+        <div className="profile-box box">
+          <div className="profile-picture-container">
+            {profilePicturePreview && (
+              <img src={profilePicturePreview} alt="Profile" className="profile-picture" />
+            )}
+            <label htmlFor="file-upload" className="custom-file-upload">
+              Choose File
+            </label>
+            <input id="file-upload" type="file" onChange={handleFileChange} />
+          </div>
+          <input type="text" name="name" value={profile.name} onChange={handleChange} placeholder="Name" />
+        </div>
+        <div className="company-info box">
+          <div className="header">Company Information</div>
+          <input
+            type="text"
+            name="companyInformation"
+            value={profile.companyInformation}
+            onChange={handleChange}
+            placeholder="Add your company information here"
+          />
+        </div>
+        <div className="InternCards">
+          <Popular />
+        </div>
+        <div className="NewIntern">
+          <button className="NewAdd">ADD NEW</button>
+        </div>
       </div>
     </div>
   );
