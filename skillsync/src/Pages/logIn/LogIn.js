fad0ac64639a12221f7dd757ba1c5a7acc4b6b7d
kushana <rashmisenadheera98@gmail.com>
2024-07-16 23:48:43 +0530

    updated login page

--- skillsync/src/Pages/logIn/LogIn.js
+++ skillsync/src/Pages/logIn/LogIn.js
@@ -41,12 +41,12 @@ export const LogIn = () => {
     <>
       <div className="flex-container">
         <div className="flex-box01">
-          <div className="logo2">logo</div>
+        <div className="logo">InternX</div>
           <div className="welcome">Welcome to</div>
           <div className="internx">InternX Community</div>
         </div>
 
-        <div className="flex-box2">
+        <div className="flex-box02">
           <div className="login">
             <h5>Login to your account</h5>
             <div>
