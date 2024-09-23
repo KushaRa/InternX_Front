89518bbbd5cc7fa586e50d71bfc6df4ebfc024c9
kushana <rashmisenadheera98@gmail.com>
2024-09-23 16:26:41 +0530

    updates

--- skillsync/src/components/Hero/Hero.jsx
+++ skillsync/src/components/Hero/Hero.jsx
@@ -15,7 +15,7 @@ export const Hero = () => {
                 Discover your perfect internship effortlessly, while IT companies find top talent seamlessly.
                 Join InternX today and unlock boundless opportunities.</p>
             <div className='heroLoginbtn'>
-              <Link to="/selectuser"><button type="button">Sign in</button></Link>
+              <Link to="/selectuser"><button type="button">Sign up</button></Link>
             </div>
     </div>
   );
